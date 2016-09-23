ae9365a11bae8257c8b817e9174c8f5019dfb3dd
dclark87 <danieljclark87@gmail.com>
2016-09-22 22:49:40 -0400

    Added unique chars string methods and tests

--- src/main/java/io/github/dclark87/javatools/arraysstrings/Strings.java
+++ src/main/java/io/github/dclark87/javatools/arraysstrings/Strings.java
@@ -1,8 +1,63 @@
 package io.github.dclark87.javatools.arraysstrings;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- *
+ * Class with string methods
  */
 public class Strings {
 
+    /**
+     * Method to test if a string has all unique characters using a HashMap
+     *
+     * @param string string to test for uniqueness
+     * @return boolean
+     */
+    public static boolean hasAllUniqueCharsHashMap(String string) {
+        // Init a new hash map
+        Map<Character, Integer> charCountMap = new HashMap<Character, Integer>();
+
+        // Iterate through the characters of string
+        for (int i = 0; i < string.length(); i++) {
+            char charInString = string.charAt(i);
+            if (charCountMap.containsKey(charInString)) {
+                return false;
+            } else {
+                charCountMap.put(charInString, 1);
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Method to test if a string has all unique characters using a HashMap
+     *
+     * @param string string to test for uniqueness
+     * @return boolean
+     */
+    public static boolean hasAllUniqueCharsBitMask(String string) {
+
+        // If the string has greater than unique ASCII chars, return false
+        if (string.length() > 128) {
+            return false;
+        }
+
+        // Init bit mask
+        int bitMaskChecker = 0;
+
+        // Iterate through the characters of string
+        for (int i = 0; i < string.length(); i++) {
+
+            // Convert character to integer
+            int val = Character.getNumericValue(string.charAt(i));
+            int charBitMask = 1 << val;
+
+            // If 1's match up in bit mask - it's not unique
+            if ((bitMaskChecker & charBitMask) > 0) return false;
+            // Otherwise, update bit mask
+            bitMaskChecker |= charBitMask;
+        }
+        return true;
+    }
 }
--- src/test/java/io/github/dclark87/javatools/arraysstrings/StringsTest.java
+++ src/test/java/io/github/dclark87/javatools/arraysstrings/StringsTest.java
@@ -1,8 +1,32 @@
 package io.github.dclark87.javatools.arraysstrings;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
- *
+ * Test class for the Strings class
  */
 public class StringsTest {
 
+    // Init variables
+    private final String nonDupsString = "abcde";
+    private final String dupsString = "aabbccddee";
+
+    /**
+     * Test string has all unique characters using hash map method
+     */
+    @Test
+    public void testHasAllUniqueCharsHashMap() {
+        Assert.assertTrue(Strings.hasAllUniqueCharsHashMap(nonDupsString));
+        Assert.assertFalse(Strings.hasAllUniqueCharsHashMap(dupsString));
+    }
+
+    /**
+     * Test string has all unique characters using bit mask method
+     */
+    @Test
+    public void hasAllUniqueCharsBitMask() {
+        Assert.assertTrue(Strings.hasAllUniqueCharsBitMask(nonDupsString));
+        Assert.assertFalse(Strings.hasAllUniqueCharsBitMask(dupsString));
+    }
 }
