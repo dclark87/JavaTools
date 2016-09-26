86aeec51b961b56886b1893ddd47179536163949
dclark87 <danieljclark87@gmail.com>
2016-09-25 22:49:21 -0400

    Added merge sort method and test case

--- src/main/java/io/github/dclark/javatools/sortsearch/Sort.java
+++ src/main/java/io/github/dclark/javatools/sortsearch/Sort.java
@@ -1,10 +1,9 @@
 package io.github.dclark.javatools.sortsearch;
 
 /**
- * Created by Danny on 9/25/16.
+ * Class with sorting methods
  */
 public class Sort {
-
 
     /**
      * Sort an array via the merge-sort algorithm
@@ -23,5 +22,38 @@ public class Sort {
         Comparable[] first = new Comparable[array.length/2];
         Comparable[] second = new Comparable[array.length-first.length];
 
+
+        // Copy elements to new arrays
+        System.arraycopy(array, 0, first, 0, first.length);
+        System.arraycopy(array, first.length, second, 0, second.length);
+
+        // Recursively call to break into smaller pieces
+        mergeSort(first);
+        mergeSort(second);
+
+        // Once we've got the first and second arrays sorted, iterate through them
+        int firstIdx = 0;
+        int secondIdx = 0;
+        int mergedIdx = 0;
+
+        while (firstIdx < first.length && secondIdx < second.length) {
+            // If first half element is smaller, put that
+            if (first[firstIdx].compareTo(second[secondIdx]) < 0) {
+                array[mergedIdx] = first[firstIdx];
+                firstIdx++;
+            }
+            else {
+                array[mergedIdx] = second[secondIdx];
+                secondIdx++;
+            }
+            mergedIdx++;
+        }
+
+        // Copy remainder of first and second lists into array (only one will have any copied in)
+        System.arraycopy(first, firstIdx, array, mergedIdx, first.length-firstIdx);
+        System.arraycopy(second, secondIdx, array, mergedIdx, second.length-secondIdx);
+
+        // Return the sorted array
+        return array;
     }
 }
