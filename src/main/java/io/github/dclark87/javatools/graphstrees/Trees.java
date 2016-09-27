f5d9da0b9cea109583205ff9aae2c1336b05ff84
dclark87 <danieljclark87@gmail.com>
2016-09-26 23:02:40 -0400

    Added initial binary tree classes and tests

--- src/main/java/io/github/dclark87/javatools/graphstrees/Trees.java
+++ src/main/java/io/github/dclark87/javatools/graphstrees/Trees.java
@@ -1,7 +1,53 @@
 package io.github.dclark87.javatools.graphstrees;
 
+import lombok.Data;
+
 /**
  * Class for parsing and manipulating trees
  */
 public class Trees {
+
+    /**
+     * Binary Tree Node class
+     * @param <T> any type of data to store in tree
+     */
+    @Data
+    public static class BinaryTreeNode<T> {
+        private T data;
+        private BinaryTreeNode<T> leftChild;
+        private BinaryTreeNode<T> rightChild;
+
+        /**
+         * Constructor for node with data
+         * @param data data payload to store in node
+         */
+        public BinaryTreeNode (T data) {
+            this.data = data;
+            this.leftChild = null;
+            this.rightChild = null;
+        }
+    }
+
+    /**
+     * Binary tree class
+     */
+    public static class BinaryTree<T> {
+
+        BinaryTreeNode<T> head;
+
+        /**
+         * Binary Tree empty constructor
+         */
+        BinaryTree() {
+            this.head = null;
+        }
+
+        /**
+         * Binary Tree single head node constructor
+         * @param data data type to store in the head of the tree
+         */
+        BinaryTree(T data) {
+            this.head = new BinaryTreeNode<T>(data);
+        }
+    }
 }
--- src/test/java/io/github/dclark87/javatools/graphstrees/TreesTest.java
+++ src/test/java/io/github/dclark87/javatools/graphstrees/TreesTest.java
@@ -1,7 +1,22 @@
 package io.github.dclark87.javatools.graphstrees;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
- * Created by Danny on 9/26/16.
+ * Test case for the Trees class
  */
 public class TreesTest {
+
+    /**
+     * Test use of binary tree and nodes classes
+     */
+    @Test
+    public void testBinaryTree() {
+        // Test both constructors
+        Trees.BinaryTree binaryTree = new Trees.BinaryTree<Integer>();
+        binaryTree.head = new Trees.BinaryTreeNode<Integer>(0);
+        Trees.BinaryTree binaryTree2 = new Trees.BinaryTree<Integer>(0);
+        Assert.assertEquals(binaryTree.head.getData(), binaryTree2.head.getData());
+    }
 }
